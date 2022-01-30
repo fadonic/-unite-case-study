1b95a58ec857dc5345a4261ae7b7529fcd457d3d
unknown <fadonic@yahoo.com>
2022-01-30 19:03:36 +0100

    Added test case for Header component

--- src/__test__/Header.test.tsx
+++ src/__test__/Header.test.tsx
@@ -3,10 +3,11 @@ import { render, screen, cleanup, fireEvent } from '@testing-library/react'
 import '@testing-library/jest-dom'
 import Header from '../../js/header'
 
+
 const handleNewTodoKeyDown = jest.fn()
 
 describe('Header', () => {
-  it('should render header component without any error', async () => {
+  it('should render header component without error', async () => {
     render(<Header handleNewTodoKeyDown={handleNewTodoKeyDown}/>)
     const headingElement = screen.getByText(/todos/i)
     expect(headingElement).toBeInTheDocument()
